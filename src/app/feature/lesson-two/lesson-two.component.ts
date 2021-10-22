0f63855e8de56c039a5abd6bd3d93c2358c732f5
belogod <belogod@gmail.com>
2021-10-22 15:52:40 +0300

    Add Game

--- src/app/feature/lesson-two/lesson-two.component.ts
+++ src/app/feature/lesson-two/lesson-two.component.ts
@@ -7,7 +7,9 @@ import {Component, OnInit} from '@angular/core';
 })
 export class LessonTwoComponent implements OnInit {
   public answerComputer = '';
+  public yourResult = '';
   public title = 'Two Lesson';
+  public stage = '1';
 
   constructor() {
   }
@@ -15,16 +17,48 @@ export class LessonTwoComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  playGame() {
+  playGame(answer: string) {
     const computerChoice = Math.random();
 
     if (computerChoice < 0.34) {
       this.answerComputer = 'камень';
+      if (answer === 'камень') {
+        this.yourResult = 'Ничья'
+      }
+      if (answer === 'ножницы') {
+        this.yourResult = 'Вы проиграли'
+      }
+      if (answer === 'бумага') {
+        this.yourResult = 'Вы победили'
+      }
     } else if (computerChoice <= 0.67) {
       this.answerComputer = 'бумага';
+      if (answer === 'камень') {
+        this.yourResult = 'Вы проиграли'
+      }
+      if (answer === 'ножницы') {
+        this.yourResult = 'Вы победили'
+      }
+      if (answer === 'бумага') {
+        this.yourResult = 'Ничья'
+      }
     } else {
       this.answerComputer = 'ножницы';
+      if (answer === 'камень') {
+        this.yourResult = 'Вы победили'
+      }
+      if (answer === 'ножницы') {
+        this.yourResult = 'Ничья'
+      }
+      if (answer === 'бумага') {
+        this.yourResult = 'Вы проиграли'
+      }
     }
+
     console.log('this.answerComputer', this.answerComputer)
+  }
+
+  changeRound(st: string) {
+    this.stage = st;
   }
 }

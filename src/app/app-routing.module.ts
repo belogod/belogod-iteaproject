fe68d920b4c3ac7c2e7e8c52e34d63b616989d26
belogod <belogod@gmail.com>
2021-10-25 12:54:09 +0300

    Redirrect **

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import {RouterModule, Routes} from '@angular/router';
 export const ROUTES: Routes = [
   {path: 'lesson-one', loadChildren: () => import ('./feature/lesson-one/lesson-one.module').then(m => m.LessonOneModule)},
   {path: 'lesson-two', loadChildren: () => import ('./feature/lesson-two/lesson-two.module').then(m => m.LessonTwoModule)},
-  {path: 'lesson-two-answer', loadChildren: () => import ('./feature/lesson-two-answer/lesson-two-answer.module').then(m => m.LessonTwoAnswerModule)}
+  {path: 'lesson-two-answer', loadChildren: () => import ('./feature/lesson-two-answer/lesson-two-answer.module').then(m => m.LessonTwoAnswerModule)},
+  {path: '**', redirectTo: 'lesson-one'}
 ];
 
 @NgModule({

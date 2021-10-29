b7e5f4262c29ce1212e1f6e28d516371d7db6572
belogod <belogod@gmail.com>
2021-10-29 09:07:26 +0300

    no message

--- src/app/feature/lesson-three-string-pipe/lesson-three-string-pipe.component.ts
+++ src/app/feature/lesson-three-string-pipe/lesson-three-string-pipe.component.ts
@@ -1,17 +1,22 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Output} from '@angular/core';
 
 @Component({
   selector: 'app-lesson-three-string-pipe',
   templateUrl: './lesson-three-string-pipe.component.html',
   styleUrls: ['./lesson-three-string-pipe.component.css']
 })
+
 export class LessonThreeStringPipeComponent {
 
   public title = 'Lesson Three - String Pipe';
 
-  public custDir: '';
-  public revStrPipe: '';
-  public revStr: '';
+  public custDir = '';
+  public revStrPipe = 'kitty';
+
+  public revStr = 'qwerty';
   public revInt = 3;
+
+  @Output() allNumbers: string = 'all';
+  @Output() positiveNumbers: string = 'positive';
 
 }
--- src/app/feature/lesson-three-string-pipe/positive-numbers.directive.ts
+++ src/app/feature/lesson-three-string-pipe/positive-numbers.directive.ts
@@ -1,28 +1,40 @@
-import {Directive, ElementRef, HostListener} from '@angular/core';
+import {Directive, ElementRef, HostListener, Input} from '@angular/core';
 
 @Directive({
   selector: '[appPositiveNumbers]'
 })
 export class PositiveNumbersDirective {
 
-  private regex: RegExp = new RegExp(/^-?[13579]+(\.[13579]*){0,1}$/g);
+  @Input() regex: string;
+
+  public regexPositiveNumbers: RegExp = new RegExp(/^-?[13579]+(\.[13579]*){0,1}$/g);
+  public regexAllNumbers: RegExp = new RegExp(/^-?[1234567890]{0,1}$/g);
+
   private specialKeys: Array<string> = ['Backspace', 'Tab'];
 
-  constructor (
+  constructor(
     private el: ElementRef
   ) {
   }
 
   @HostListener('keydown', ['$event'])
+
   onKeyDown(event: KeyboardEvent) {
     if (this.specialKeys.indexOf(event.key) !== -1) {
       return;
     }
     const current: string = this.el.nativeElement.value;
     const next: string = current.concat(event.key);
-    if (next && !String(next).match(this.regex)) {
-      event.preventDefault();
+    if (this.regex === 'all') {
+      if (next && !String(next).match(this.regexAllNumbers)) {
+        event.preventDefault();
+      }
+    } else {
+      if (next && !String(next).match(this.regexPositiveNumbers)) {
+        event.preventDefault();
+      }
     }
+
   }
 
 }

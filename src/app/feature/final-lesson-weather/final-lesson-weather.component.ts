0af3af6fa4384f7509709994e1094fe16eeed8b9
belogod <belogod@gmail.com>
2021-11-04 18:54:43 +0200

    no message

--- src/app/feature/final-lesson-weather/final-lesson-weather.component.ts
+++ src/app/feature/final-lesson-weather/final-lesson-weather.component.ts
@@ -1,15 +1,85 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, DoCheck, OnInit, ViewChild} from '@angular/core';
+import {LessonFourListBooksService} from "../lesson-four-list-books/lesson-four-list-books.service";
+import {MatMenuTrigger} from "@angular/material/menu";
 
 @Component({
   selector: 'app-final-lesson-weather',
   templateUrl: './final-lesson-weather.component.html',
   styleUrls: ['./final-lesson-weather.component.scss']
 })
-export class FinalLessonWeatherComponent implements OnInit {
+export class FinalLessonWeatherComponent implements OnInit, DoCheck {
 
-  constructor() { }
+  public title: string = 'Weather';
+
+  public search: string = 'Kyiv';
+  public visible: boolean = false;
+  public count: Array<string> = [];
+  public units: string = 'metric';
+  public roundTemp: number;
+  public feels_like: number;
+  public temp_max: number;
+  public temp_min: number;
+
+  public allWeather: any;
+  public date = new Date();
+
+  @ViewChild(MatMenuTrigger, {static: false}) menuTrigger: MatMenuTrigger;
+
+  constructor(
+    public lessonFourListBooksService: LessonFourListBooksService
+  ) {
+    // this.getWeather();
+    // this.getWeatherMonth();
+  }
 
   ngOnInit(): void {
   }
 
+  ngDoCheck() {
+    this.allWeather = this.lessonFourListBooksService.weatherData;
+    // this.roundTemp = Math.round(this.allWeather.main.temp);
+    // this.feels_like = Math.round(this.allWeather.main.feels_like);
+    // this.temp_max = Math.round(this.allWeather.main.temp_max);
+    // this.temp_min = Math.round(this.allWeather.main.temp_min);
+    this.roundTemp = Math.round(15.54);
+    this.feels_like = Math.round(15.34);
+    this.temp_max = Math.round(12.54);
+    this.temp_min = Math.round(10.54);
+  }
+
+  // getWeather(): void {
+  //   this.lessonFourListBooksService.getWeather();
+  // }
+
+  getWeatherMonth(): void {
+    this.lessonFourListBooksService.getWeatherMonth();
+  }
+
+  someMethod() {
+    this.menuTrigger.openMenu();
+  }
+
+  applyFilter(filterValue: string) {
+    this.count = [];
+
+    if (this.units || this.units !== '') {
+      this.count.push('1');
+    }
+
+    this.visible = this.count.length > 0;
+
+    this.menuTrigger.closeMenu();
+
+    filterValue = filterValue.replace('#', '%23');
+
+    this.lessonFourListBooksService.getWeather(
+      filterValue,
+      this.units,
+    );
+
+  }
+
+  getIconUrl(img: string) {
+    return 'http://openweathermap.org/img/w/' + img + '.png'
+  }
 }
--- src/app/feature/lesson-four-list-books/lesson-four-list-books.service.ts
+++ src/app/feature/lesson-four-list-books/lesson-four-list-books.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
@@ -8,9 +8,19 @@ import {HttpClient} from "@angular/common/http";
 export class LessonFourListBooksService {
   public url: any = {
     main: 'assets/json/books.json',
+    weather: 'https://community-open-weather-map.p.rapidapi.com/weather?',
+    weatherMonth: 'https://community-open-weather-map.p.rapidapi.com/climate/month',
+    qwer: 'https://ip-geo-location.p.rapidapi.com/ip/23.123.12.11'
   }
 
   public data: any;
+  public weatherData: any;
+  public httpOptions = {
+    headers: new HttpHeaders({
+      'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
+      'x-rapidapi-key': 'bdc0162670msh434d6f318fbeee0p1cd2aejsn988fed57ed14'
+    })
+  };
 
   constructor(
     public http: HttpClient
@@ -22,6 +32,31 @@ export class LessonFourListBooksService {
       .subscribe(
         (data: any) => {
           this.data = data.content;
+        });
+  }
+
+  getWeather(filterValue: string, units: string): any {
+    if (units) {
+      units = '&units=' + units;
+    }
+    if (filterValue) {
+      filterValue = '&q=' + filterValue + '%2CUA';
+    }
+
+    return this.http.get(this.url.weather + filterValue + '&lang=ru' + units + '&mode=', this.httpOptions)
+      .subscribe(
+        (data: any) => {
+          this.weatherData = data;
+          console.log('1111', data)
+        });
+  }
+
+  getWeatherMonth(): any {
+    return this.http.get(this.url.qwer + '?format: \'json\'', this.httpOptions)
+      .subscribe(
+        (data: any) => {
+          this.weatherData = data;
+          console.log('weatherData', data)
         });
   }
 

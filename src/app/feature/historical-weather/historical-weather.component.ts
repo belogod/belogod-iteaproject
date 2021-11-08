15efe13dd9ddc56e87b881bf9f5373accedec3be
belogod <belogod@gmail.com>
2021-11-08 10:10:03 +0200

    no message

--- src/app/feature/historical-weather/historical-weather.component.ts
+++ src/app/feature/historical-weather/historical-weather.component.ts
@@ -1,24 +1,17 @@
-import {Component, DoCheck, OnInit, ViewChild} from '@angular/core';
+import {Component, DoCheck} from '@angular/core';
 import {LessonFourListBooksService} from "../lesson-four-list-books/lesson-four-list-books.service";
-import {MatMenuTrigger} from "@angular/material/menu";
 
 @Component({
   selector: 'app-historical-weather',
   templateUrl: './historical-weather.component.html',
-  styleUrls: ['../final-lesson-weather/final-lesson-weather.component.scss']
+  styleUrls: ['./historical-weather.component.scss']
 })
 export class HistoricalWeatherComponent implements DoCheck {
 
   public title: string = 'Historical Weather';
 
-  public search: string = 'Kyiv';
-  public visible: boolean = false;
-  public count: Array<string> = [];
-  public units: string = 'metric';
-
   public historicalWeather: any;
 
-  @ViewChild(MatMenuTrigger, {static: false}) menuTrigger: MatMenuTrigger;
 
   constructor(
     public lessonFourListBooksService: LessonFourListBooksService
@@ -29,10 +22,6 @@ export class HistoricalWeatherComponent implements DoCheck {
 
   ngDoCheck() {
     this.historicalWeather = this.lessonFourListBooksService.historicalWeatherData;
-  }
-
-  someMethod() {
-    this.menuTrigger.openMenu();
   }
 
   getIconUrl(img: string) {
